e100e7d2712c595405120358d4ec3a2acf9f60ac
Adam Cummings <adcummin@cisco.com>
2023-01-30 17:18:07 +0000

    some small debugging improvements

--- src/components/glastonbury/Glastonbury.vue
+++ src/components/glastonbury/Glastonbury.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="w-full flex flex-col items-center">
-        <div class="max-w-6xl bg-slate-200 w-full relative flex flex-col items-center rounded-xl p-5 pt-10">
+        <div class="max-w-6xl bg-slate-200 w-full relative flex flex-col items-center rounded-xl p-5 pt-20">
             <h2 class="text-5xl mb-5">Glastonbury Festival</h2>
-            <!-- <a v-bind:href="auth()"> -->
-
-                <span @click="auth()" class="rounded-full bg-green-700 text-white font-bold absolute top-10 right-10 py-4 px-6"
+                <span @click="auth()" class="rounded-full bg-green-700 text-white font-bold absolute top-20 right-10 py-4 px-6"
                     >S</span>
-            <!-- </a> -->
+                <div class="absolute top-3 right-3 flex gap-1">
+                    <div class="bg-blue-200 p-1 text-xs rounded text-blue-600">Token Requests:<span class="ml-1 text-red-blue bold">{{ tokenRequests }}</span></div>
+                    <div class="bg-green-200 p-1 text-xs rounded text-green-600">Track Requests:<span class="ml-1 text-green-600 bold">{{ trackRequests }}</span></div>
+                    <button @click="createNewRequest" class="bg-green-600 p-1 text-xs rounded"><span class="ml-1 text-white bold">Refresh Track Tokens</span></button>
+                </div>
             <section class="bg-white rounded-lg w-full p-5">
-                <div id="result">{{ results }}</div>
+                <button @click="showJSON = !showJSON" class="bg-blue-200 p-1 text-xs rounded text-blue-600">Show/Hide Track Response</button>
+                <div id="result" v-show="showJSON">{{ results }}</div>
                 <div id="pyramid" class="flex w-full flex-col">
                     <h3 class="text-2xl mr-10">Pyramid</h3>
                     <div class="flex flex-col bg-gray-400 p-5 rounded-md" v-for="day in lineup">
@@ -39,16 +42,25 @@ export default {
         return {
             artists: [],
             results: {},
-            lineup: {}
+            lineup: {},
+            tokenRequests: 0,
+            trackRequests: 0,
+            showJSON: false
         }
     },
     methods: {
-        init() {;
+        init() {
+            this.tokenRequests = window.sessionStorage.getItem("tokenRequests");
+            this.trackRequests = window.sessionStorage.getItem("trackRequests");
             this.results = JSON.parse(JSON.stringify(this.results))
-            console.log(this.results);
             this.checkAuth()
         },
         checkAuth() {
+            const cachedTracks = window.sessionStorage.getItem("cachedSpotifyLibrary");
+            if (cachedTracks) {
+                this.interpret(JSON.parse(cachedTracks))
+                return;
+            }
             const accessToken = window.sessionStorage.getItem("spotifyWebAPIAccessToken");
             if (accessToken) { this.getArtistData(); }
             else if (window.location.search) {
@@ -59,6 +71,13 @@ export default {
                     var xhr = new XMLHttpRequest();
 
                     xhr.onload = function() {
+                        this.tokenRequests = window.sessionStorage.getItem("tokenRequests");
+                        if (!this.tokenRequests) {
+                            window.sessionStorage.setItem("tokenRequests", 1);
+                        } else {
+                            console.log('setting new token value...')
+                            window.sessionStorage.setItem("tokenRequests", this.tokenRequests + 1);
+                        }
                         var response = xhr.response;
                         var message;
 
@@ -70,7 +89,6 @@ export default {
                             message = "Error: " + response.error_description + " (" + response.error + ")";
                         }
 
-                        console.log(response)
                         document.getElementById("result").innerHTML = message;
                     };
                     xhr.responseType = 'json';
@@ -88,22 +106,31 @@ export default {
         },
         getArtistData() {
             if (window.sessionStorage.getItem("spotifyWebAPIAccessToken")) {
-                console.log('below is sessions storage:')
-                console.log(window.sessionStorage.getItem("spotifyWebAPIAccessToken"))
                 const accessToken = window.sessionStorage.getItem("spotifyWebAPIAccessToken");
                 let offset = 0;
+                let batch = 0;
                 // while (offset < 50) {
-                fetch(`https://api.spotify.com/v1/me/tracks?limit=50&offset=${offset}`, {
-                    method: 'GET',
-                    headers: {
-                        "Content-Type": 'application/json',
-                        'Authorization': `Bearer ${accessToken}`,
-                    },
-                })
-                    .then(response => response.json())
-                    .then(response => { this.interpret(response); window.sessionStorage.setItem("cachedSpotifyLibrary", response); })
-                    .catch(err => console.error(err));
-                offset += 50;
+                    this.trackRequests = window.sessionStorage.getItem("trackRequests");
+                    console.log('track requets:'  + this.trackRequests)
+                        if (!this.trackRequests) {
+                            window.sessionStorage.setItem("trackRequests", 1);
+                        } else {
+                            console.log('setting new request value...')
+                            window.sessionStorage.setItem("trackRequests", this.trackRequests + 1);
+                        }
+                    fetch(`https://api.spotify.com/v1/me/tracks?limit=50&offset=${offset}`, {
+                        method: 'GET',
+                        headers: {
+                            "Content-Type": 'application/json',
+                            'Authorization': `Bearer ${accessToken}`
+                        },
+                    })
+                        .then(response => response.json())
+                        .then(res => { window.sessionStorage.setItem(`cachedSpotifyLibrary_${batch}`, JSON.stringify(res)); return res; })
+                        .then(response => { this.interpret(response); })
+                        .catch(err => console.error(err));
+                    batch++;
+                    offset += 50;
                 // }
             }
         },
@@ -122,7 +149,7 @@ export default {
                 })
                 .then(function(codeChallenge) {
                     
-                    var redirectUri = 'http://127.0.0.1:5173/callback';
+                    var redirectUri = 'http://localhost:5173/callback';
                     var args = new URLSearchParams({
                         response_type: "code",
                         client_id: 'c6ead95860334243aabf554648943aa7',
@@ -152,15 +179,17 @@ export default {
             }, {});
             return uniqs;
         },
+        createNewRequest() {
+            this.trackRequests = window.sessionStorage.setItem("trackRequests", 0);
+            window.sessionStorage.removeItem("spotifyWebAPIAccessToken");
+            window.sessionStorage.removeItem("cachedSpotifyLibrary");
+        },
         syncToClashFinder() {
-            //console.log(this.$data.lineup)
-
             for (let [spotifyArtist, count] of Object.entries(this.$data.results)) {
                 for (let [index, day] of Object.entries(this.$data.lineup)) {
                     for (let [index, stage] of Object.entries(day.Stages)) {
                         for (let [index, artist] of Object.entries(stage.Artists)) {
                             if (spotifyArtist.toLocaleLowerCase() == artist.Name.toLocaleLowerCase()) {
-                                console.log('match found!');
                                 artist.listens = 1;
                             }
                         }
