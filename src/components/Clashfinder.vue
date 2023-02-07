70195b6fab25cfd773f84dd291c6e70b7862249e
Adam Cummings <adcummin@cisco.com>
2023-02-07 17:12:10 +0000

    some small changes for testing

--- src/components/Clashfinder.vue
+++ src/components/Clashfinder.vue
@@ -4,7 +4,7 @@
 </script>
 
 <template>
-  <div>
+  <div v-if="setList.length !== 0">
     {{ setList }}
   </div>
   <button @click="colour = !colour" :class="colour ? 'bg-red-500' : 'bg-blue-500'">test</button>
@@ -15,14 +15,24 @@
     props: { festival: String },
     data() {
       return {
-        setList: Array,
+        setList: [],
         colour: true
       }
     },
     methods: {
       async getSetlist() {
         // pt.1 read json and send to DOM
-        this.setList = await fetch(`${this.festival}.json`).then(raw => { return raw.json() })
+        
+        this.setList = await fetch(`${this.festival}.json`)
+        .then(response => { 
+          if (response.status === 404) {
+            return ['404']
+          }
+          return response.json() 
+        })
+        .catch(error => {
+          return ['error']
+        });
       },
     },
     updated() {
