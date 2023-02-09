251bdd2bc1cd56aced98a7b61f2bcf0f564f9494
Adam Cummings <adam_cummings_@outlook.com>
2023-02-09 14:50:36 +0000

    added vis-timeline

--- src/App.vue
+++ src/App.vue
@@ -16,42 +16,8 @@
         <option value="leeds-2022">Leeds Festival 2022</option>
       </select>
     </div>
-    <Clashfinder :festivalData="festivalData" :festival="festival" :error="error" />
+    <!-- <Clashfinder :festivalData="festivalData" :festival="festival" :error="error" /> -->
   </main>
 </template>
 
-<script>
-  export default {
-    data() {
-      return {
-        festival: null,
-        festivalData: [],
-        error: null
-      }
-    },
-    mounted() {
-      //testing only
-      this.festival = 'glastonbury-2022'
-      this.switchClashfinder(this.festival)
-    },
-    methods: {
-      async switchClashfinder(input) {
-        // pt.1 read json and send to DOM
-        const url = input ? input : this.festival
-        this.festivalData = await fetch(`${url}.json`)
-        .then(response => { 
-          if (response.status === 404) {
-            this.error = `Sorry, this festival doesn't have an available clashfinder.`
-            return []
-          }
-          this.error = null;
-          return response.json() 
-        })
-        .catch(error => {
-          this.error = `Sorry, there seems to be a bug. Please report this to the developer.`
-          console.log(error)
-        });
-      },
-    },
-  }
-</script>
+
