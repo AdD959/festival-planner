3671407170c2821c3688dd2205589fb1b71c9d87
Adam Cummings <adam_cummings_@outlook.com>
2023-03-05 17:55:15 +0000

    more minor updates

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,11 @@ import Clashfinder from './components/clashfinder/Clashfinder.vue'
 
 
 <template>
-  <div class="relative text-skin-inverted max-w-[1080px]">
-    <h1 class="text-5xl font-bold mt-20">Festival Planner</h1>
-    <Buttons @festivalSelect="festivalSelected"/>
+  <div class="flex flex-col w-full">
+    <div class="relative text-skin-inverted max-w-[1080px]">
+      <h1 class="text-5xl font-bold mt-20">Festival Planner</h1>
+      <Buttons @festivalSelect="festivalSelected"/>
+    </div>
     <Clashfinder :festival="currentFestival"/>
   </div>
   <TestBar v-show="env === 'dev'" />
--- src/components/clashfinder/Clashfinder.vue
+++ src/components/clashfinder/Clashfinder.vue
@@ -1,9 +1,10 @@
 <script setup>
-    import Error from '../errors/Generic.vue'
+import Error from '../errors/Generic.vue'
 </script>
 
 <template>
-    <Error msg="Sorry, this clashfinder isn't currently available." :showError="error"/>
+    <div id="container"></div>
+    <Error msg="Sorry, this clashfinder isn't currently available." :showError="error" />
 </template>
 
 <script>
@@ -12,6 +13,8 @@ export default {
     data() {
         return {
             error: false,
+            timelines: [],
+            containers: [],
         }
     },
     updated() {
@@ -28,7 +31,63 @@ export default {
                     }
                     this.error = true
                 })
-            // .then(json => { this.createTable(json); return json })
+                .then(json => {
+                    if (!this.error) {
+                        this.createTable(json)
+                    }
+                })
+        },
+        createTable(json) {
+            if (document.querySelector(`#${this.festival}`)) {
+                return
+            } else {
+                const div = document.createElement('div')
+                div.id = this.festival
+                document.querySelector('#container').appendChild(div)
+            }
+            var groups = [
+                { id: 1, content: 'Pyramid' },
+                { id: 2, content: 'Other' },
+                { id: 3, content: 'West Holts' },
+                { id: 4, content: 'John Peel' },
+                { id: 5, content: 'The Park' },
+                { id: 6, content: 'Acoustic' }
+            ]
+
+            var options = [
+                {
+                    min: '2022-02-16 10:30:00',
+                    max: '2022-02-17 01:00:00',
+                    zoomFriction: 50,
+                    zoomMin: 3000000,
+                    multiselect: true,
+                    showTooltips: true,
+                },
+                {
+                    min: '2022-02-17 10:30:00',
+                    max: '2022-02-18 01:00:00',
+                    zoomFriction: 50,
+                    zoomMin: 3000000,
+                    multiselect: true,
+                    showTooltips: true,
+                },
+                {
+                    min: '2022-02-18 10:30:00',
+                    max: '2022-02-19 01:00:00',
+                    zoomFriction: 50,
+                    zoomMin: 3000000,
+                    multiselect: true,
+                    showTooltips: true,
+                }
+            ]
+
+            json.forEach((day, i) => {
+                let dataset = new vis.DataSet(day.Artists)
+                let container = document.querySelector(`#${this.festival}`)
+                this.containers.push(container)
+                const timeline = new vis.Timeline(this.containers[i], dataset, groups, options[i])
+                this.timelines.push(timeline)
+            })
         }
     }
 }
